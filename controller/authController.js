50ebbffe099958b1ebcd4613c3cb64011322ef32
cemedisball <cemedisbod@gmail.com>
2024-08-24 15:48:25 +0700

    updated

--- controller/authController.js
+++ controller/authController.js
@@ -1,56 +1,64 @@
-const express = require('express');
-const jwt = require("jsonwebtoken");
-const bcrypt = require ("bcryptjs");
-const User = require("../models/user");
 
-//Register
-exports.register = async (req,res) => {
-    const { user_name , password,name,role} = req.body;
-    try{
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ user_name, password: hashedPassword,name,role});
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const dotenv = require('dotenv');
+dotenv.config();
+
+//REGISTER 
+exports.register = async (req, res) => {
+    const { user_name, password } = req.body;
+    try {
+        const hashedPasswor = await bcrypt.hash(password, 10);
+        const user = new User({ user_name, password: hashedPasswor });
         await user.save();
         res.status(201).send("User registered");
-    }catch (err){
+    } catch (err) {
         res.status(400).send(err.message);
     }
+
 };
 
-//login
-exports.login = async (req,res) =>{
-    const {user_name ,password} = req.body;
-    try{
-    const user = await User.findOne ({user_name});
-    if(!user) return res.status(400).send("User not found");
-    const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch) return res.status(400).send("Invalid credentials");
+//LOGIN 
+exports.login = async (req, res) => {
+    const { user_name, password } = req.body;
+    try {
+        const user = await User.findOne({ user_name });
+        if (!user) return res.status(400).send("User not found");
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).send("Invalid credentials");
 
-    const accessToken = jwt.sign(
-        { userId : user._id},
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn :"5m"}
-    );
-    const refreshToken = jwt.sign(
-        {userId: user._id},
-        process.env.REFRESH_TOKEN_SECRET,
-    );
-    res.json({accessToken, refreshToken});
-    } catch (err){
+        const accessToken = jwt.sign(
+            { userId: user._id },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "10m" }
+        );
+        const refreshToken = jwt.sign(
+            { userId: user._id },
+            process.env.REFRESH_TOKEN_SECRET
+        );
+        res.json({ accessToken, refreshToken });
+
+    } catch (err) {
         res.status(500).send(err.message);
     }
+
 };
 
-//refresh
-exports.refresh = async (req,res) =>{
-    const {token} = req.body;
-    if(!token) return res.sendStatus(401);
-    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err,user)=>{
+
+exports.refresh = async (req, res) => {
+    const { token } = req.body;
+
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
-            const accessToken = jwt.sign(
-                {userId : user.userId},
-                process.env.ACCESS_TOKEN_SECRET,
-                {expiresIn: '15m'}
-            );
-            res.json ({accessToken}); 
-    });
-};
+        const accessToken = jwt.sign(
+            { userID: user.userId },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "15m" }
+        );
+        res.json({ accessToken });
+    })
+}
